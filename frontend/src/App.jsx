f60b6ca8f47bff7c4cea3352df51eba29b0017ef
polkboban <polkboban@gmail.com>
2025-08-04 23:03:43 +0530

    deploy2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch('https://tny1ink.onrender.com', {
+      const res = await fetch('https://tny1ink.onrender.com/api/shorten', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url }),
